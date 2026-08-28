41941118b0450287fadd6a76243721fe9bd2bebd
alden <63831735+aldenasf@users.noreply.github.com>
2026-08-28 13:13:23 +0700

    feat(register): add functionality to register

---
+++ js/register.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const registerForm = document.getElementById("register");
+
+    registerForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const name = formData.get("name");
+        const email = formData.get("email");
+        const password = formData.get("password");
+        const passwordConfirm = formData.get("password-confirm");
+        const agreement = formData.get("agreement");
+
+        if (!name) return formError("Nama kosong");
+
+        if (!email.length || !validateEmail(email))
+            return formError("Email tidak valid");
+
+        if (!password.length) return formError("Password kosong");
+
+        if (password !== passwordConfirm)
+            return formError("Password tidak cocok");
+
+        if (!agreement)
+            return formError(
+                "Kamu harus menyutujui Terms of Service dan Privacy Policy",
+            );
+
+        const newUser = {
+            id: users.length + 1,
+            name: name,
+            email: email,
+            password: password,
+            role: "user",
+        };
+
+        users.push(newUser);
+        console.log(users);
+
+        alert(
+            `User didaftar: \n\n${JSON.stringify(newUser)}\n\nCek console untuk melihat array users`,
+        );
+    });
+});
+
+// Fungsi untuk mengecek apakah user sudah ter login
+function isLoggedIn() {
+    const userRaw = localStorage.getItem("loginUser");
+    const user = JSON.parse(userRaw);
+
+    // Mengecek apakah `user` pada localStorage tidak kosong dan terdaftar di database
+    if (user !== undefined && users.includes(user)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function formError(message) {
+    const element = document.getElementById("error");
+
+    element.innerHTML = message;
+    element.classList.remove(["hidden"]);
+}
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
